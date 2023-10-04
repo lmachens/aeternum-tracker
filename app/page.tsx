7c3053c8e68ecdd8b4e0f6120d241df6a7e96472
lmachens <leon@machens.koeln>
2023-10-04 14:41:46 +0200

    Add tile

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ActivityReset } from "@/components/activity-reset";
 import { CustomActivities } from "@/components/custom-activities";
 import { Skeleton } from "@/components/ui/skeleton";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 
 const Activities = dynamic(
   () => import("@/components/activities").then((mod) => mod.Activities),
@@ -28,14 +29,25 @@ const Activities = dynamic(
 export default function Home() {
   return (
     <main className="flex flex-col gap-4 container grow">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Personalized Tracking for New World Players
-      </h1>
-      <p>
-        Aeternum Tracker puts you in control of your New World journey.
-        Customize and track the in-game activities and resources that matter
-        most to you. Keep tabs on your progress and optimize your gameplay.
-      </p>
+      <div className="grid grid-cols-12 items-center justify-items-center md:justify-items-end gap-4">
+        <div className="col-span-12 md:col-span-6 space-y-2 md:space-y-8">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Personalized Tracking for New World Players
+          </h1>
+          <p>
+            Aeternum Tracker puts you in control of your New World journey.
+            Customize and track the in-game activities and resources that matter
+            most to you. Keep tabs on your progress and optimize your gameplay.
+          </p>
+        </div>{" "}
+        <Image
+          src="/tile.webp"
+          alt="Aeternum Tracker"
+          width={400}
+          height={400}
+          className="col-span-12 md:col-span-6"
+        />
+      </div>
       <div className="ml-auto flex gap-2">
         <CustomActivities />
         <ActivityReset />
